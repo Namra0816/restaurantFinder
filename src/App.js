eb403f1e671d5ac33c51a2115b9cf929f49402ed
Namra0816 <namra.rc8216@gmail.com>
2024-06-09 13:51:04 -0700

    updated code to support restaurantList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Navbar from "../../restaurantFinder/src/components/Navbar/Navbar";
+import RestaurantList from './pages/restaurantList/restaurantList';
+import restaurantData from './assets/restaurantData/restaurantData.json';
 
 function App() {
+  const [restaurants] = useState(restaurantData);
+
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:city/:state" element={<RestaurantList restaurants={restaurants} />} />
       </Routes>
     </div>
   );
