8825928a64756e4ea26fbcabcaba41043db64834
Namra0816 <namra.rc8216@gmail.com>
2024-06-09 00:17:34 -0700

    added routing and Home page to app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
 import Navbar from "../../restaurantFinder/src/components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
