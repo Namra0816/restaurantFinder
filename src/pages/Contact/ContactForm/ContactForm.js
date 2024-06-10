4cfc4131f5923f715f695374c2ea39e45d7b9c8e
Namra0816 <namra.rc8216@gmail.com>
2024-06-10 02:32:07 -0700

    contactform added

---
+++ src/pages/Contact/ContactForm/ContactForm.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './ContactForm.css'
+
+const ContactForm = () => {
+  return (
+        <div className='container contact-content'>
+            <div>
+                <h1 className='heading-secondary'>
+                    Mail <span>us</span>
+                </h1>
+
+                <form>
+                    <div className='form-field name-email'>
+                        <div>
+                            <label>Name</label>
+                            <input type="text" name='name' />
+                        </div>
+
+                        <div>
+                            <label>Email</label>
+                            <input type="email" name='email' />
+                        </div>
+                    </div>
+
+
+                    <div className='form-field'>
+                        <div>
+                            <label>Subject</label>
+                            <input type="text" name='subject' />
+                        </div>
+                    </div>
+
+                    <div className='form-field'>
+                        <div>
+                            <label>Message</label>
+                            <textarea type="text" name='message' />
+                        </div>
+                    </div>
+                </form>
+
+                <button>Submit</button>
+            </div>
+        </div>
+  )
+}
+
+export default ContactForm
