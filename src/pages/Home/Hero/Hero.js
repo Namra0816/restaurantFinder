a9f7c49ca5e4c2f78ee80cca8d0dd5e502a8545d
Namra0816 <namra.rc8216@gmail.com>
2024-06-10 02:56:49 -0700

    primary heading changed

--- src/pages/Home/Hero/Hero.js
+++ src/pages/Home/Hero/Hero.js
@@ -5,7 +5,7 @@ const Hero = ({ children }) => {
   return (
         <div className='hero-content'>
             <h1 className='heading-primary'>
-                Find your <span> restaurant </span> with us
+                Find your<span> Dine </span> with us
             </h1>
             <p className='heading-secondary'>
                 Find online or call <span className='special-word'>(424)123-4567</span>
